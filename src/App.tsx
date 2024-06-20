05970c8891f04034508c4e964041bd61116c36e4
LaloG19 <21362134@utcancun.edu.mx>
2024-06-20 18:35:26 -0500

    ft: Consumo de JSONPlaceholder para diferenciar el lazy-load

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ const LazyComponent = lazy(() => import('./components/LazyComponent'));
 const App: React.FC = () => {
   return (
     <div className="App bg-[#111] min-w-full min-h-screen h-full">
-      <h1 className='bg-[#353535] text-white text-2xl uppercase tracking-[0.25rem] text-center p-4 rounded-b-3xl'> Usando useReducer y LazyLoadding </h1>
+      <h1 className='bg-[#353535] text-white text-2xl tracking-[0.25rem] text-center p-4 rounded-b-3xl'> useReducer & LazyLoadding </h1>
       <Counter />
-      <Suspense fallback={<div className='p-4 bg-red-500 absolute bottom-0 w-screen text-purple-500 font-bold tracking-[0.25rem] rounded-t-3xl'> Cargando... </div>}>
+      <Suspense fallback={<div className='p-4 bg-red-500 absolute bottom-0 w-screen text-purple-500 font-bold tracking-[0.25rem] rounded-t-3xl'>Cargando... Cargando... Cargando... </div>}>
         <LazyComponent />
       </Suspense>
     </div>
--- src/components/LazyComponent.tsx
+++ src/components/LazyComponent.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const LazyComponent: React.FC = () => {
-  return <div className="p-4 bg-[#353535] absolute bottom-0 w-full text-white font-bold tracking-[0.25rem] rounded-t-3xl"> Este componente es cargado con Lazy-Load. </div>;
+  
+  const [usuarios, setUsuarios] = React.useState<any[]>([]);
+
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => setUsuarios(data));
+  }, []);
+  
+  return (
+    <>
+      <div  
+        className="p-4 bg-[#353535] absolute 
+        bottom-0 w-full text-white font-bold 
+        tracking-[0.25rem] rounded-t-3xl"
+      >
+        { usuarios.length > 0 && <p key={usuarios[0].id}>{usuarios[0].name}</p> }
+      </div>
+    </>
+  );
 };
 
 export default LazyComponent;
