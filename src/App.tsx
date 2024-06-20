ff94d8c3ede8503db3727877f2b4094e9d1b3533
LaloG19 <21362134@utcancun.edu.mx>
2024-06-20 17:54:44 -0500

    up: Estilos al H1 y footer (lazy component)

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const LazyComponent = lazy(() => import('./components/LazyComponent'));
 const App: React.FC = () => {
   return (
     <div className="App bg-[#111] min-w-full min-h-screen h-full">
-      <h1> Usando useReducer y LazyLoadding </h1>
+      <h1 className='bg-[#353535] text-white text-2xl uppercase tracking-[0.25rem] text-center p-4 rounded-b-3xl'> Usando useReducer y LazyLoadding </h1>
       <Counter />
       <Suspense fallback={<div> Cargando... </div>}>
         <LazyComponent />
--- src/components/LazyComponent.tsx
+++ src/components/LazyComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const LazyComponent: React.FC = () => {
-  return <div className="p-4 bg-[#353535] absolute bottom-0 w-full text-white font-bold tracking-[0.25rem]"> Este componente es cargado con Lazy-Load. </div>;
+  return <div className="p-4 bg-[#353535] absolute bottom-0 w-full text-white font-bold tracking-[0.25rem] rounded-t-3xl"> Este componente es cargado con Lazy-Load. </div>;
 };
 
 export default LazyComponent;
